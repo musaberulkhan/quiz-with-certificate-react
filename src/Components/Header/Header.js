16aef45001f60045cf49ca67c8e9a332cf6f8c5f
Ahmed Musaberul Khan <musaberulkhan@gmail.com>
2021-09-28 21:24:54 +0600

    Bootstrap, Google Font and Header Added

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Participate Programming Quiz and <span>Judge Your Skills!</span> </h1>
+            <p>Make it work, make it right, make it fast</p>
+        </div>
+    );
+};
+
+export default Header;
